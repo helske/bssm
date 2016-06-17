4c27acd82764fda037ca9f5bacebaa293673892a
Jouni Helske <jouni.helske@jyu.fi>
2016-06-17 17:45:49 +0300

    more tests

--- tests/testthat/test_bsm.R
+++ tests/testthat/test_bsm.R
@@ -42,3 +42,32 @@ test_that("different smoothers give identical results",{
   expect_error(out_bssm2 <- fast_smoother(model_bssm), NA)
   expect_equivalent(out_bssm2, out_bssm1$alphahat)
 })
+
+
+test_that("MCMC results are correct",{
+  set.seed(123)
+  model_bssm <- bsm(rnorm(10,3), P1 = diag(2,2), sd_level = 2)
+
+  expect_error(out <- run_mcmc(model_bssm, n_iter = 5), NA)
+
+  testvalues <- structure(c(
+    -21.1188759509962, -20.9618902905707, -20.5927926916911),
+    .Dim = c(3L, 1L))
+  expect_equivalent(testvalues, out$logLik)
+
+  testvalues <- structure(c(
+    1.07190610570214, 0.880984917781493, 0.869554983660202,
+    2.16848541839909, 2.2395102983541, 2.07258174715106),
+    .Dim = c(3L, 2L), .Dimnames = list(NULL, c("sd_y", "sd_level")),
+    mcpar = c(3, 5, 1), class = "mcmc")
+  expect_equivalent(testvalues, out$theta)
+
+  testvalues <- c(
+    1.400889326126, 1.90882445483498,
+    0.192057468590735, 1.97901949320989,
+    -0.971428708875681, 0.645910489882064)
+  expect_equivalent(testvalues, out$alpha[c(1,10,20,25, 31, 60)])
+
+})
+
+
--- tests/testthat/test_ng_bsm.R
+++ tests/testthat/test_ng_bsm.R
@@ -32,3 +32,35 @@ test_that("results are comparable to KFAS",{
   expect_equivalent(out_KFAS$V, out_bssm$Vt)
 })
 
+test_that("MCMC results are correct",{
+  set.seed(123)
+  model_bssm <- ng_bsm(rpois(10,3), P1 = diag(2,2),
+    sd_level = 2, phi = 22:31, distribution = "poisson")
+
+  expect_error(out <- run_mcmc(model_bssm, n_iter = 5), NA)
+
+  testvalues <- structure(
+    c(-32.0549000294351, -29.7677152890342, -30.6035140701869),
+    .Dim = c(3L, 1L))
+  expect_equivalent(testvalues, out$logLik)
+
+  testvalues <- structure(c(1.92994662744397, 1.92144209410961, 1.92311870965196
+  ), .Dim = c(3L, 1L), .Dimnames = list(NULL, "sd_level"), mcpar = c(3,
+    5, 1), class = "mcmc")
+  expect_equivalent(testvalues, out$theta)
+
+  testvalues <- c(-1.88946906575704, -2.27440000655681, -0.0889180484569738,
+    -1.7798944305367, -0.516822307442448, 0.388771271837778)
+  expect_equivalent(testvalues, out$alpha[c(1,10,20,25, 31, 60)])
+
+  expect_error(out2 <- run_mcmc(model_bssm, n_iter = 5, nsim_states = 10), NA)
+
+  testvalues <- c(
+    -1.53451331164171, -1.65374734086221,
+    -0.61385163351857, -1.18098534825777,
+    -0.61385163351857, -0.61385163351857)
+  expect_equivalent(testvalues, out2$alpha[c(1,10,20,25, 31, 60)])
+
+})
+
+
