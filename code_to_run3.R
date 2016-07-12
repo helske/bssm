264df3562f914d4c60c2f638b10aa03822e6a995
Jouni Helske <jouni.helske@jyu.fi>
2016-07-12 09:36:36 +0300

    correct target acceptance in DA2

--- code_to_run3.R
+++ code_to_run3.R
@@ -46,7 +46,7 @@ is_da_comparison <- function(nsim = 1000, n_iter = 1e4, n_burnin = 0, nsim_state
       summary(res$theta)$stat[, 4]^2)
 
     s <- system.time(res <- run_mcmc(model, n_iter = n_iter, n_burnin = n_burnin,
-      nsim_states = nsim_states, target = res$acceptance_rate / 0.234, method = "delayed acceptance"))[3]
+      nsim_states = nsim_states, target = 0.234^2 / res$acceptance_rate, method = "delayed acceptance"))[3]
     results[4, , i] <-   c(s, res$acceptance_rate,
       rowMeans(res$alpha[500,,]),
       summary(coda::mcmc(t(res$alpha[500,,])))$stat[, 4]^2,
