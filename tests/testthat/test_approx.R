ef23d5121183aa702eeb2bb448dc7fc2a3e9127d
Jouni Helske <jouni.helske@jyu.fi>
2016-09-27 11:28:06 +0100

    added space

--- tests/testthat/test_approx.R
+++ tests/testthat/test_approx.R
@@ -1,7 +1,7 @@
 context("Test Gaussian approximation")
 
 test_that("results for poisson GLM are equal to glm function",{
-  d <- data.frame(treatment= gl(3,3), outcome = gl(3,1,9), counts = c(18,17,15,20,10,20,25,13,12))
+  d <- data.frame(treatment = gl(3,3), outcome = gl(3,1,9), counts = c(18,17,15,20,10,20,25,13,12))
   glm_poisson <- glm(counts ~ outcome + treatment, data = d, family = poisson())
   xreg <- model.matrix(~ outcome + treatment, data = d)
   expect_error(model_poisson <- ngssm(d$counts, Z = t(xreg), T = diag(5), R = diag(0, 5), P1 = diag(1e7, 5), 
