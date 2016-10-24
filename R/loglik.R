0e1222c89b2709a5fddd00027be19d69975c4eac
Jouni Helske <jouni.helske@jyu.fi>
2016-10-24 16:03:50 +0100

    document method in loglik

--- R/loglik.R
+++ R/loglik.R
@@ -5,6 +5,10 @@
 #' @param object Model object.
 #' @param nsim_states Number of samples for importance sampling. If 0, approximate log-likelihood is returned.
 #' See vignette for details.
+#' @param method Method for computing the log-likelihood of non-Gaussian/non-linear model. 
+#' Default \code{"spdk"} uses the importance sampling approach by 
+#' Shephard and Pitt (1997), and Durbin and Koopman (1997). \code{"psi"} uses psi-auxiliary filter and 
+#' \code{"bootstrap"} bootstrap filter.
 #' @param seed Seed for the random number generator. Compared to other functions of the package, the
 #' default seed is fixed (as 1) in order to work properly in numerical optimization algorithms.
 #' @param max_iter Maximum number of iterations.
