1daa06b148bdf2b69d1e372dc14c80c5f653c070
Jouni Helske <jouni.helske@jyu.fi>
2016-10-24 15:24:02 +0100

    change convtol for KFAS

--- tests/testthat/test_basics.R
+++ tests/testthat/test_basics.R
@@ -34,6 +34,7 @@ test_that("different smoothers give identical results",{
 
 test_that("results for poisson model are comparable to KFAS",{
   library("KFAS")
+  set.seed(1)
   model_KFAS <- SSModel(rpois(10, exp(0.2) * (2:11)) ~ SSMtrend(2, Q = list(0.01^2, 0)),
     distribution = "poisson", u = 2:11)
   model_KFAS$P1inf[] <- 0
@@ -42,7 +43,7 @@ test_that("results for poisson model are comparable to KFAS",{
   model_bssm <- ng_bsm(model_KFAS$y, P1 = diag(1e2,2), sd_slope = 0,
     sd_level = 0.01, u = 2:11, distribution = "poisson")
   
-  expect_equal(logLik(model_KFAS,convtol = 1e-12), logLik(model_bssm,0))
+  expect_equal(logLik(model_KFAS), logLik(model_bssm, 0))
   out_KFAS <- KFS(model_KFAS, filtering = "state", convtol = 1e-12)
   expect_error(out_bssm <- kfilter(model_bssm), NA)
   expect_equivalent(out_KFAS$a, out_bssm$at)
@@ -55,6 +56,7 @@ test_that("results for poisson model are comparable to KFAS",{
 
 test_that("results for binomial model are comparable to KFAS", {
   library("KFAS")
+  set.seed(1)
   model_KFAS <- SSModel(rbinom(10, 2:11, 0.4) ~ SSMtrend(2, Q = list(0.01^2, 0)),
     distribution = "binomial", u = 2:11)
   model_KFAS$P1inf[] <- 0
@@ -63,7 +65,7 @@ test_that("results for binomial model are comparable to KFAS", {
   model_bssm <- ng_bsm(model_KFAS$y, P1 = diag(1e2,2), sd_slope = 0,
     sd_level = 0.01, u = 2:11, distribution = "binomial")
   
-  expect_equal(logLik(model_KFAS,convtol = 1e-12), logLik(model_bssm,0))
+  expect_equal(logLik(model_KFAS), logLik(model_bssm, 0))
   out_KFAS <- KFS(model_KFAS, filtering = "state", convtol = 1e-12)
   expect_error(out_bssm <- kfilter(model_bssm), NA)
   expect_equivalent(out_KFAS$a, out_bssm$at)
