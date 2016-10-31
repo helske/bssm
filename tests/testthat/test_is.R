2ff36d936b640e34f3cc0d5d27a55fbc01a34e65
Jouni Helske <jouni.helske@jyu.fi>
2016-10-31 15:13:02 +0000

    modify testvalues due to antithetics

--- tests/testthat/test_is.R
+++ tests/testthat/test_is.R
@@ -13,8 +13,8 @@ test_that("Test that poisson ng_bsm give identical results with ngssm",{
     distribution = "poisson"), NA)
   expect_error(sim_ng_bsm <- importance_sample(model_ng_bsm, 4, seed = 2), NA)
   expect_equal(sim_ng_bsm, sim_ngssm)
-  testvalues <- structure(c(1.33288345256985, 0.979030809094139, 1.73347913263807, 
-    0.312770093194118), .Dim = c(4L, 1L))
+  testvalues <- structure(c(1.33288345256985, 0.979030809094139, 0.326927291153189, 
+    0.954068461233847), .Dim = c(4L, 1L))
   expect_equal(sim_ng_bsm$weights, testvalues)
 })
 
@@ -24,8 +24,8 @@ test_that("Test that svm still works",{
     sd_ar = halfnormal(0.2, 5), sigma = halfnormal(1, 2))
   
   expect_error(sim <- importance_sample(model, 4, seed = 2), NA)
-  testvalues <- structure(c(5.86702179982002, 0.215967278599225, 0.0735471187105569, 
-    0.64491606343298), .Dim = c(4L, 1L))
+  testvalues <- structure(c(5.86702179982002, 0.215967278599225, 0.0131080574094302, 
+0.666553357641113), .Dim = c(4L, 1L))
   expect_equal(sim$weights, testvalues)
   
 })
--- tests/testthat/test_mcmc.R
+++ tests/testthat/test_mcmc.R
@@ -12,13 +12,14 @@ test_that("MCMC results for gaussian model are correct",{
   expect_error(identical(run_mcmc(model_bssm, n_iter = 10, nsim_states = 5, seed = 1)[-8], 
     run_mcmc(model_bssm, n_iter = 10, nsim_states = 5, seed = 1)[-8]), NA)
   
-  testvalues <- structure(c(-23.2747514859484, -23.4917121873825, -23.2613263226018), 
-    .Dim = c(3L, 1L))
+  testvalues <- structure(c(-23.2747514859484, -23.4917121873825, -23.2613263226018
+  ), .Dim = c(3L, 1L))
   expect_equivalent(testvalues, out$posterior)
   
   testvalues <- structure(c(0.834199058228936, 0.975961440040881, 0.836899897389733, 
-    1.01308074724915, 0.883852023501684, 0.991296915615001), .Dim = c(3L, 2L), 
-    .Dimnames = list(NULL, c("sd_y", "sd_level")), mcpar = c(3, 5, 1), class = "mcmc")
+    1.01308074724915, 0.883852023501684, 0.991296915615001), .Dim = c(3L, 
+      2L), .Dimnames = list(NULL, c("sd_y", "sd_level")), mcpar = c(3, 
+        5, 1), class = "mcmc")
   expect_equivalent(testvalues, out$theta)
   
   testvalues <- c(1.97371748764053, 2.79020761994165, 0.100672315364547, 3.63895784712199, 
@@ -37,17 +38,17 @@ test_that("MCMC results for Poisson model are correct",{
   expect_error(identical(run_mcmc(model_bssm, n_iter = 10, nsim_states = 5)[-8], 
     run_mcmc(model_bssm, n_iter = 10, nsim_states = 5)[-8]), NA)
   
-  testvalues <- structure(c(-39.2859389395516, -39.2859389395516, -39.2859389395516, 
-    -37.8081239720664, -37.846333182345), .Dim = c(5L, 1L))
+  testvalues <- structure(c(-39.3880935409099, -37.3318580117567, -37.6177815203621, 
+    -38.8795560250992, -38.8795560250992), .Dim = c(5L, 1L))
   expect_equivalent(testvalues, out$posterior)
   
-  testvalues <- structure(c(2.09962020707956, 2.09962020707956, 2.09962020707956, 
-    1.49453752077392, 1.63439378916187), .Dim = c(5L, 1L), .Dimnames = list(
+  testvalues <- structure(c(1.94161122336829, 1.45629893808125, 1.54833882701089, 
+    1.86010843337335, 1.86010843337335), .Dim = c(5L, 1L), .Dimnames = list(
       NULL, "sd_level"), mcpar = c(6, 10, 1), class = "mcmc")
   expect_equivalent(testvalues, out$theta)
   
-  testvalues <- c(-2.25051173382636, 0.401204374024394, -1.29957037760506, 0.70044534511549, 
-    -1.29957037760506, -1.29957037760506)
+  testvalues <- c(-0.179742059090531, 0.190378831062351, -0.0451489240338361, 
+    0.582554939438518, -0.0149794944120587, -0.42743617132243)
   expect_equivalent(testvalues, out$alpha[c(1,10,20,25, 31, 60)])
 })
 
@@ -61,17 +62,17 @@ test_that("MCMC results for binomial model are correct",{
   expect_error(identical(run_mcmc(model_bssm, n_iter = 10, nsim_states = 5)[-8], 
     run_mcmc(model_bssm, n_iter = 10, nsim_states = 5)[-8]), NA)
   
-  testvalues <- structure(c(-38.4510899893105, -38.4510899893105, -38.4510899893105, 
-    -38.4510899893105, -38.2787954724052), .Dim = c(5L, 1L))
+  testvalues <- structure(c(-37.5274138423666, -37.5274138423666, -37.5274138423666, 
+    -37.5898467281197, -37.5898467281197), .Dim = c(5L, 1L))
   expect_equivalent(testvalues, out$posterior)
   
-  testvalues <- structure(c(2.0499145967585, 2.0499145967585, 2.0499145967585, 
-    2.0499145967585, 2.03647937119522), .Dim = c(5L, 1L), .Dimnames = list(
+  testvalues <- structure(c(1.79609530935165, 1.79609530935165, 1.79609530935165, 
+    1.80278017969165, 1.80278017969165), .Dim = c(5L, 1L), .Dimnames = list(
       NULL, "sd_level"), mcpar = c(6, 10, 1), class = "mcmc")
   expect_equivalent(testvalues, out$theta)
   
-  testvalues <- c(-0.129365208071896, -0.0764425225893368, 0.754866067120236, 
-    -0.0826085226842411, 0.754866067120236, 0.754866067120236)
+  testvalues <- c(-0.467178624242946, -0.216527057264181, 0.977319983890701, 
+    1.04226099911296, 0.977319983890701, 0.977319983890701)
   expect_equivalent(testvalues, out$alpha[c(1,10,20,25, 31, 60)])
 })
 
@@ -87,16 +88,16 @@ test_that("MCMC results for negative binomial model are correct",{
   expect_error(identical(run_mcmc(model_bssm, n_iter = 10, nsim_states = 5)[-8], 
     run_mcmc(model_bssm, n_iter = 10, nsim_states = 5)[-8]), NA)
   
-  testvalues <- structure(c(-263.765826199904, -263.765826199904, -261.661097641368, 
-    -262.375150730155, -262.375150730155), .Dim = c(5L, 1L))
+  testvalues <- structure(c(-259.408148930098, -260.865064698319, -261.14000172789, 
+    -261.14000172789, -262.097829940278), .Dim = c(5L, 1L))
   expect_equivalent(testvalues, out$posterior)
   
-  testvalues <- structure(c(1.16553263865597, 1.16553263865597, 1.35971289542647, 
-    1.28435318292968, 1.28435318292968), .Dim = c(5L, 1L), .Dimnames = list(
+  testvalues <- structure(c(1.78355552962326, 1.4643447573198, 1.4252130704839, 
+    1.4252130704839, 1.3120054169815), .Dim = c(5L, 1L), .Dimnames = list(
       NULL, NULL), mcpar = c(16, 20, 1), class = "mcmc")
   expect_equivalent(testvalues, out$theta)
   
-  expect_equivalent(1.49337944854536, out$alpha[1])
+  expect_equivalent(1.52626449167856, out$alpha[1])
 })
 
 
@@ -109,20 +110,19 @@ test_that("MCMC results for SV model are correct",{
   expect_error(identical(run_mcmc(model_bssm, n_iter = 10, nsim_states = 5)[-8], 
     run_mcmc(model_bssm, n_iter = 10, nsim_states = 5)[-8]), NA)
   
-  testvalues <- structure(c(-17.2785499634381, -17.1522385567832, -17.2408376389957, 
-    -17.2408376389957, -17.2529363969), .Dim = c(5L, 1L))
+  testvalues <- structure(c(-18.644214166485, -18.644214166485, -18.644214166485, 
+    -18.14059171039, -17.7427882105074), .Dim = c(5L, 1L))
   expect_equivalent(testvalues, out$posterior)
   
-  testvalues <- structure(c(0.611645727466498, 0.784126255249186, 0.739990910881518, 
-    0.739990910881518, 0.538608198317944, 0.132471223920487, 0.260180586666946, 
-    0.305401598068018, 0.305401598068018, 0.449188287046116, 0.732778575928857, 
-    0.80046492329671, 0.817714439472359, 0.817714439472359, 1.02447292408947
+  testvalues <- structure(c(0.881217648431571, 0.881217648431571, 0.881217648431571, 
+    0.75116941472834, 0.684070011216901, 0.463435906249714, 0.463435906249714, 
+    0.463435906249714, 0.549836717261301, 0.353421831153397, 1.56694018288237, 
+    1.56694018288237, 1.56694018288237, 1.3097950653206, 1.23093038495726
   ), .Dim = c(5L, 3L), .Dimnames = list(NULL, c("rho", "sd_ar", 
     "sigma")), mcpar = c(16, 20, 1), class = "mcmc")
   expect_equivalent(testvalues, out$theta)
   
-  testvalues <- c(-0.0830748149852014, -0.198578873285639, 0.178440933808639, 
-    0.0256754390526584)
+  testvalues <- c(-0.857317424752647, -0.53536238357548, -0.53536238357548, -0.0228423519266524)
   expect_equivalent(testvalues, out$alpha[c(1,10, 20, 50)])
 })
 
@@ -146,14 +146,14 @@ test_that("MCMC results for SV model using IS-correction are correct",{
     run_mcmc(model_bssm, n_iter = 100, nsim_states = 10, method = "isc", 
       simulation_method = "psi")[-8]), NA)
   testvalues <- structure(c(-19.0505020531966, -19.3483555773888, -19.2742098743496, 
--17.9587002271749), .Dim = c(4L, 1L))
+    -17.9587002271749), .Dim = c(4L, 1L))
   expect_equivalent(testvalues, out$posterior)
   
   testvalues <- structure(c(0.741988771923731, 0.697340092900775, 0.628452176724537, 
-0.379346527686448, 1.19914545826558, 1.46878142788352, 1.56475733617601, 
-1.41912717643135, 1.02106720262573, 0.5352855667731, 0.556124259401741, 
-0.617628937905398), .Dim = c(4L, 3L), .Dimnames = list(NULL, 
-    c("rho", "sd_ar", "sigma")))
+    0.379346527686448, 1.19914545826558, 1.46878142788352, 1.56475733617601, 
+    1.41912717643135, 1.02106720262573, 0.5352855667731, 0.556124259401741, 
+    0.617628937905398), .Dim = c(4L, 3L), .Dimnames = list(NULL, 
+      c("rho", "sd_ar", "sigma")))
   expect_equivalent(testvalues, out$theta)
   
   testvalues <- c(-0.357482373598444, 0.239536835878186, 1.39616333824964)
