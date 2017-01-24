dae56053a770a357158f7a4ac4ba8bcc88584549
Jouni Helske <jouni.helske@jyu.fi>
2017-01-24 13:50:43 +0000

    conditional distribution of states

--- src/conditional_dist.cpp
+++ src/conditional_dist.cpp
@@ -1,15 +1,15 @@
-#include <RcppArmadillo.h>
+#include "conditional_dist.h"
 
 // [[Rcpp::export]]
-void conditional_dist_helper(arma::cube& Vt, arma::cube& Ct) {
+void conditional_cov(arma::cube& Vt, arma::cube& Ct) {
   
   unsigned int p = Vt.n_cols;
   for (int t = Vt.n_slices - 1; t > 0; t--) {
     arma::mat U(p, p);
-    arma::mat V(p, p);
+    arma::mat V(1, 1); //not using this
     arma::vec s(p);
     arma::svd_econ(U, s, V, Vt.slice(t - 1), "left");
-     arma::uvec nonzero = arma::find(s > (arma::datum::eps * p * s(0)));
+    arma::uvec nonzero = arma::find(s > (arma::datum::eps * p * s(0)));
     arma::mat tmp = Ct.slice(t - 1).t() * U.cols(nonzero) * 
       arma::diagmat(1.0 / s(nonzero)) * U.cols(nonzero).t();
     Vt.slice(t) -= tmp * Ct.slice(t - 1);
@@ -21,7 +21,7 @@ void conditional_dist_helper(arma::cube& Vt, arma::cube& Ct) {
     
   }
   arma::mat U(p, p);
-  arma::mat V(p, p);
+  arma::mat V(1, 1); //not using this
   arma::vec s(p);
   arma::svd_econ(U, s, V, Vt.slice(0), "left");
   
