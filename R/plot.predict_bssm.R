473321823d799a011c055439d0f40188b65304cc
Jouni Helske <jouni.helske@jyu.fi>
2017-03-06 19:49:28 +0200

    fixed intervals and ts start

--- R/plot.predict_bssm.R
+++ R/plot.predict_bssm.R
@@ -15,31 +15,32 @@ autoplot.predict_bssm <- function(object, plot_mean = TRUE,
   plot_median = TRUE, y, fit,
   obs_color = "black", mean_color = "red", median_color = "blue",
   fit_color = mean_color, interval_color = "#000000", alpha_fill = 0.25,
-   ...) {
-
+  ...) {
+  
   if (!missing(y) && !is.ts(y)) {
-    y <- ts(y, start = start(object$mean), frequency = frequency(object$mean))
+    y <- ts(y, end = end(object$mean), frequency = frequency(object$mean))
   }
   
   plot_y <- !missing(y)
   if (!missing(fit) && !is.ts(fit)) {
-    fit <- ts(fit, start = start(object$mean), frequency = frequency(object$mean))
+    fit <- ts(fit, end = end(object$mean), frequency = frequency(object$mean))
   }
   plot_fit <- !missing(fit)
-  d <- ts.union(if (plot_y) y, if (plot_mean) object$mean, object$intervals, 
-    if (plot_fit) fit)
+  d <- ts.union(if (plot_y) y,  if (plot_fit) fit, if (plot_mean) object$mean, 
+    object$intervals)
   #d[nrow(d) - length(object$mean), 2:(ncol(d) - plot_fit)] <-  d[nrow(d) - length(object$mean), 1]
   times <- seq(from = tsp(d)[1], to = tsp(d)[2], by = 1/tsp(d)[3])
   d <- data.frame("time" = times, d)
-  names(d) <-  c("time", if (plot_y) "y", if (plot_mean) "mean",
-    paste0("a",sub("%","", colnames(object$intervals))), if (plot_fit) "fit")
-
+  names(d) <-  c("time", if (plot_y) "y", if (plot_fit) "fit", 
+    if (plot_mean) "mean", paste0("a",sub("%","", colnames(object$intervals))))
+  
   n_mean <- length(object$mean)
   p <- ggplot(tail(d, n_mean), aes(time, mean)) +
-   # geom_line(colour = mean_color) +
+    # geom_line(colour = mean_color) +
     scale_x_continuous(limits = range(times)) +
     scale_y_continuous(limits = range(d[,-1], na.rm = TRUE))
-  intv <- names(d)[-(1:(plot_y + 1 + plot_mean))]
+  intv <- names(d)[-(1:(1 + plot_y + plot_fit + plot_mean))]
+  
   n_intvs <- floor(length(intv)/2)
   
   for (i in 1:n_intvs) {
