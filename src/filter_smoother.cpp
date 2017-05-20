77f90286a35bae81fa6ddca7167c468d4186df37
Jouni Helske <jouni.helske@jyu.fi>
2017-05-20 16:45:45 +0100

    correct regspace

--- src/filter_smoother.cpp
+++ src/filter_smoother.cpp
@@ -4,7 +4,7 @@
 
 void filter_smoother(arma::cube& alpha, const arma::umat& indices) {
   
-  arma::uvec b(alpha.n_slices) = arma::regspace<arma::uvec>(0, alpha.n_slices - 1);
+  arma::uvec b = arma::regspace<arma::uvec>(0, alpha.n_slices - 1);
   
   for (int t = alpha.n_cols - 2; t >= 0; t--) {
     arma::mat alphatmp = alpha.tube(arma::span::all, arma::span(t));
