e21aae1363701aaa199b75dd461ca20f05e4c9c8
Jouni Helske <jouni.helske@iki.fi>
2018-01-05 12:07:46 +0100

    add ar1 model

---
+++ src/ugg_ar1.cpp
@@ -0,0 +1,64 @@
+#include "ugg_ar1.h"
+
+// from Rcpp::List
+ugg_ar1::ugg_ar1(const Rcpp::List& model, const unsigned int seed) :
+  ugg_ssm(model, seed), 
+  mu_est(Rcpp::as<bool>(model["mu_est"])), 
+  sd_y_est(Rcpp::as<bool>(model["sd_y_est"])) {
+}
+
+void ugg_ar1::update_model(const arma::vec& new_theta) {
+  
+  
+  T(0, 0, 0) = new_theta(0);
+  R(0, 0, 0) = new_theta(1);
+  if (mu_est) {
+    a1(0) = new_theta(2);
+    C.fill(new_theta(2) * (1.0 - new_theta(0)));
+  }
+  P1(0, 0) = std::pow(new_theta(1), 2) / (1.0 - std::pow(new_theta(0), 2));
+  
+  compute_RR();
+  
+  if(sd_y_est) {
+    H(0) = new_theta(2 + mu_est);
+    HH(0) = H(0);
+  }
+  
+  if(xreg.n_cols > 0) {
+    beta = new_theta.subvec(new_theta.n_elem - xreg.n_cols, new_theta.n_elem - 1);
+    compute_xbeta();
+  }
+  theta = new_theta;
+}
+
+double ugg_ar1::log_prior_pdf(const arma::vec& x) const {
+  
+  double log_prior = 0.0;
+  
+  for(unsigned int i = 0; i < x.n_elem; i++) {
+    switch(prior_distributions(i)) {
+    case 0  :
+      if (x(i) < prior_parameters(0, i) || x(i) > prior_parameters(1, i)) {
+        return -std::numeric_limits<double>::infinity(); 
+      }
+      break;
+    case 1  :
+      if (x(i) < 0) {
+        return -std::numeric_limits<double>::infinity();
+      } else {
+        log_prior -= 0.5 * std::pow(x(i) / prior_parameters(0, i), 2);
+      }
+      break;
+    case 2  :
+      log_prior -= 0.5 * std::pow((x(i) - prior_parameters(0, i)) / prior_parameters(1, i), 2);
+      break;
+    }
+  }
+  return log_prior;
+}
+
+double ugg_ar1::log_proposal_ratio(const arma::vec& new_theta, const arma::vec& old_theta) const {
+  return 0.0;
+}
+
---
+++ src/ugg_ar1.h
@@ -0,0 +1,22 @@
+#ifndef UGG_AR1_H
+#define UGG_AR1_H
+
+#include "ugg_ssm.h"
+
+class ugg_ar1: public ugg_ssm {
+  
+public:
+  
+  ugg_ar1(const Rcpp::List& model, const unsigned int seed);
+  
+  // update model given the parameters theta
+  void update_model(const arma::vec& new_theta);
+  double log_prior_pdf(const arma::vec& x) const;
+  double log_proposal_ratio(const arma::vec& new_theta, const arma::vec& old_theta) const;
+  
+private:
+  const bool mu_est;
+  const bool sd_y_est;
+};
+
+#endif
