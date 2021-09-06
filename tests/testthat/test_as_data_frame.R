aa7ba116e3aaa9680f2fb43a3727be06abca5916
helske <jouni.helske@iki.fi>
2021-09-06 16:46:48 +0300

    identical to equal due to CRAN test without long doubles

--- tests/testthat/test_as_data_frame.R
+++ tests/testthat/test_as_data_frame.R
@@ -42,7 +42,7 @@ test_that("expanded and not expanded data frame work equally for states", {
   expect_error(d <- as.data.frame(mcmc_bsm, variable = "theta"), NA)
   expect_error(sumr <- summary(mcmc_bsm, variable = "both", return_se = TRUE), 
     NA)
-  expect_identical(mean(d$value[d$variable == "sd_y"]), 
+  expect_equal(mean(d$value[d$variable == "sd_y"]), 
     sumr$theta["sd_y", "Mean"])
   
 })
